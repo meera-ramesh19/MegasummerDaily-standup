fd75f8f53c653eec67948c54fce35960602aeaf9
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-07-13 20:41:22 -0400

    Update checkForImposterSyndrome.js

--- groupsolutions/checkForImposterSyndrome.js
+++ groupsolutions/checkForImposterSyndrome.js
@@ -5,6 +5,6 @@
 Write a function checkForImposterSyndrome(str) that returns true if str contains 'not ready' or "can't interview yet", otherwise false
 
 function checkForImposterSyndrome(str){
- let str=str.toLowerCase()
- return str.includes('not ready') || str.includes( "can't interview yet"))?true:false
+ let lowerCase=str.toLowerCase()
+ return  lowerCase.includes('not ready') ||  lowerCase.includes( "can't interview yet"))?true:false
 }
