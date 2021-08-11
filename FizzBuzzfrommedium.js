862a9ee99fcea7f336f3325dedf2136a2b807e38
meera-ramesh19 <46873861+meera-ramesh19@users.noreply.github.com>
2021-08-10 23:47:51 -0400

    Create FizzBuzzfrommedium.js

---
+++ FizzBuzzfrommedium.js
@@ -0,0 +1,21 @@
+Fizz Buzz
+Given a number as an input, print otu every integer from 1 to that number . 
+However when the integer is divisible by 2, print out "Fizz",
+when the integer is divisible by 2, print out "Buzz",
+when the integer is divisible by 2 and divisible by 3, print out "FizzBuzz"
+
+ 
+ 
+ 
+ function fizzbuzz(n)
+{
+   
+  for(let i=1;i<=n;i++){
+          if (i%2===0 && i%3===0){console.log('FizzBuzz')}
+             else if(i%3===0){console.log("Buzz")}
+            else if (i%2===0){console.log"Fizz")}
+            else{console.log(i)}
+
+ }
+  return arr
+}
